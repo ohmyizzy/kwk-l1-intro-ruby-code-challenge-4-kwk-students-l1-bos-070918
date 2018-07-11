654e845e00a6ff366836fe860f1689a1c2d56c0c
ohmyizzy <conneri19@barringtonschools.org>
2018-07-11 19:16:45 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -3,5 +3,13 @@
 def least_coins(cents)
 
 #Code your answer here!
-
+coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 4}
+coins[:quarters] = (cents/25).floor 
+cents -= coins[:quarters] * 25
+coins[:dimes] = (cents/10).floor
+cents -= coins[:dimes] * 10
+coins[:nickels] = (cents/5).floor
+cents -= coins[:nickels] * 5 
+coins[:pennies] = cents
+coins
 end
